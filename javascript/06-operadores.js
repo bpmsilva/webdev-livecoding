6b197fe174f020a355a42a0b6113c2e5c378bc84
Bernardo <bpmsilva@gmail.com>
2025-06-18 12:42:21 -0300

    adiciona arquivo sobre operadores em JS

---
+++ javascript/06-operadores.js
@@ -0,0 +1,12 @@
+// Operadores lógicas em JavaScript e comparação com Python
+
+// Operadores lógicos
+// Python: and, or, not
+// JavaScript: &&, ||, !
+let a = true;
+let b = false;
+
+// Comparação de operadores lógicos
+let c = a && b; // Operador AND
+let d = a || b; // Operador OR
+let e = !a; // Negação
