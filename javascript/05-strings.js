ff3c17b20b0991620b4d443dbd708262a7fa79d9
Bernardo <bpmsilva@gmail.com>
2025-06-18 09:24:49 -0300

    adiciona arquivo sobre strings

---
+++ javascript/05-strings.js
@@ -0,0 +1,18 @@
+// Strings e manipulação de strings
+// Neste exemplo, exploramos como manipular strings em JavaScript.
+
+// Criando uma string
+let minhaString = "Olá, Mundo!";
+
+// Exibindo a string no console'
+console.log(minhaString);
+
+// Obtendo o comprimento da string
+console.log("Comprimento da string:", minhaString.length);
+
+// Acessando caracteres individuais
+console.log("Primeiro caractere:", minhaString[0]);
+
+// Convertendo para maiúsculas e minúsculas
+console.log("Maiúsculas:", minhaString.toUpperCase());
+console.log("Minúsculas:", minhaString.toLowerCase());
