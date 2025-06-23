eaf7d4c1d49af1b4796352303e8510d788b40525
Bernardo <bpmsilva@gmail.com>
2025-06-23 13:30:03 -0300

    adiciona formas de utilizar loops em javascript

---
+++ javascript/10-fors-javascript.js
@@ -0,0 +1,39 @@
+// Há algumas maneiras de fazer loops em JavaScript
+
+// 1. A mais comum é o for, que é usado para iterar sobre arrays
+// ou executar um bloco de código um número específico de vezes
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// 2. O while executa um bloco de código enquanto a condição for verdadeira
+let contador = 0;
+while (contador < 5) {
+    console.log('Contador (while):', contador);
+    contador++;
+}
+
+// Um array para os próximos exemplos
+let numeros = [1, 2, 3, 4, 5];
+
+// 3. Também podemos usar o for...of para iterar sobre arrays
+for (let numero of numeros) {
+    console.log(numero);
+}
+
+// 4. Ou o for...in para iterar sobre objetos
+let pessoa = { nome: "João", idade: 30, cidade: "São Paulo" };
+for (let chave in pessoa) {
+    console.log(`${chave}: ${pessoa[chave]}`);
+}
+
+// 5. Podemos usar o forEach para iterar sobre arrays
+numeros.forEach(function(numero) {
+    console.log(numero);
+});
+
+// 6. Ou usar o map para criar um novo array com base em um array existente
+let quadrados = numeros.map(function(numero) {
+    return numero * numero;
+});
+console.log(quadrados);
